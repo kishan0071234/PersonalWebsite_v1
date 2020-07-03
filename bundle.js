f566d00ab53c9e22bacc2f12d78ff68f384bac14
agarwak1 <KAgarwal@esurance.com>
2020-07-03 03:52:50 -0700

    Updates

--- bundle.js
+++ bundle.js
@@ -91262,6 +91262,11 @@ var projects = [{
   link: "https://github.com/kishan0071234/Stack-Overflow"
 }, {
   id: 3,
+  description: "Personal Website",
+  readme: "https://github.com/kishan0071234/PersonalWebsite_v1/blob/master/README.md",
+  link: "https://github.com/kishan0071234/PersonalWebsite_v1"
+}, {
+  id: 4,
   description: "Deep Learning Multiple Regression",
   readme: "https://github.com/kishan0071234/USA-Inflation-Prediction/blob/master/README.md",
   link: "https://github.com/kishan0071234/USA-Inflation-Prediction"
