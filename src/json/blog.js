c303b6d242594559b780d0c3be5bd11e487574d3
kishan Agarwal <kishan007agarwal@gmail.com>
2020-07-03 14:57:25 -0700

    Update blog.js

--- src/json/blog.js
+++ src/json/blog.js
@@ -6,7 +6,7 @@ const blogs = [
     link:
       "https://medium.com/@kishan007agarwal/secure-service-to-service-communication-using-public-key-cryptography-55da3c640a22",
     image: "https://miro.medium.com/max/554/1*ipcrE8iwwv-DX2GEMyeWOA.png",
-    platform: "MEDIUM",
+    platform: "MEDIUMS",
     url: "src/images/medium.png",
   },
 ];
